3bb0926031af895d488cb4a89ecc209eea170969
kriti goyal <goyalkriti262005@gmail.com>
2025-10-01 22:40:51 +0530

    changed import directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
-import SignUp from "./pages/SignUp"; 
+import SignUp from "./pages/SignUp/SignUp"; 
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
